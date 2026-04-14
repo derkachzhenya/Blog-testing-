f0f4bd0047e266cc31be9ea6e6e87ee50257892b
zhenya457 <zheka4573@gmail.com>
2026-04-14 20:46:09 +0300

    updating

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,22 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
-    protected $table = 'posts';
-
-    protected $guarded = false;
-
-    public function category(): BelongsTo
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function tags(): BelongsToMany
-    {
-        return $this->belongsToMany(Tag::class, 'post_tags')
-            ->withTimestamps();
+        return $this->belongsTo(User::class)->withDefault([
+            'name' => 'Guest Author',
+        ]);
     }
 }
--- database/migrations/2026_04_14_081814_create_posts_table.php
+++ database/migrations/2026_04_14_081814_create_posts_table.php
@@ -6,24 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
-            $table->timestamps();
+            // ❌ missing delete behavior
+            $table->foreignId('user_id')->constrained();
 
+            // ✅ safe
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('posts');
