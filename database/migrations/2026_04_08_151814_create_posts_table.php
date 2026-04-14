611fb23ea5fb9298ff9499bbae918ef0696d2847
zhenya457 <zheka4573@gmail.com>
2026-04-14 20:46:37 +0300

    updating

--- database/migrations/2026_04_08_151814_create_posts_table.php
+++ database/migrations/2026_04_08_151814_create_posts_table.php
@@ -6,23 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            // ❌ missing delete behavior
-            $table->foreignId('user_id')->constrained();
-
-            // ✅ safe
-            $table->foreignId('user_id')
-                ->constrained()
-                ->cascadeOnDelete();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
+
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('posts');
